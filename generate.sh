65c45c55cbb076c17eb98335883abccf422b07ea
Nathaël NOGUÈS <nathael.nogues@gmail.com>
2022-04-16 19:23:08 +0200

    Pushed buyChunks 1.0.0

---
+++ generate.sh
@@ -0,0 +1,11 @@
+APPDATA="~/AppData/Roaming/shapez.io"
+
+# Get version (line starting with "version:" in the file)
+NAME='buyChunks'
+
+SRC="src/$NAME.js"
+
+VERSION=$( grep 'version:' "$SRC" | head -n 1 | grep -o '\([0-9]\+\.\)\+[0-9]' )
+DST="mods/$NAME@$VERSION.js"
+
+curl -X POST -s --data-urlencode "input@$SRC" https://www.toptal.com/developers/javascript-minifier/raw > "$DST"
