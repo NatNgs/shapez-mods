0f4e6440ccd10dee2d0e6825f075cb6b60a6902b
Nathaël NOGUÈS <nathael.nogues@gmail.com>
2022-04-21 18:43:54 +0200

    Generate release zip automatically

--- generate.sh
+++ generate.sh
@@ -1,4 +1,5 @@
 APPDATA="~/AppData/Roaming/shapez.io"
+ZIP="c:/Program Files/7-Zip/7z.exe"
 
 # Get version (line starting with "version:" in the file)
 NAME='buyChunks'
@@ -6,6 +7,9 @@ NAME='buyChunks'
 SRC="src/$NAME.js"
 
 VERSION=$( grep 'version:' "$SRC" | head -n 1 | grep -o '\([0-9]\+\.\)\+[0-9]' )
-DST="generated/$NAME@$VERSION.js"
+DST="$NAME@$VERSION.js"
 
-curl -X POST -s --data-urlencode "input@$SRC" https://www.toptal.com/developers/javascript-minifier/raw > "$DST"
+curl -X POST -s --data-urlencode "input@$SRC" https://www.toptal.com/developers/javascript-minifier/raw > "generated/$DST"
+
+cd generated
+"$ZIP" a -mx9 "$NAME-$VERSION.zip" "$DST"
